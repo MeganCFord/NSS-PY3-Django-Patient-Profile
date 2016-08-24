c3e17f24a26ba26b20c52355980de80659a1b25c
Megan Ford <meg.c.ford@gmail.com>
2016-08-24 10:15:20 -0500

    added join tables with dates.

--- patientprofiles/admin.py
+++ patientprofiles/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 
-from .models import Patient, Ailment, Medication
+from .models import Patient, Ailment, Medication, PatientAilment, PatientAilmentMedication
 
 # Register your models here.
 admin.site.register(Ailment)
 admin.site.register(Medication)
 admin.site.register(Patient)
+admin.site.register(PatientAilment)
+admin.site.register(PatientAilmentMedication)
 
 
 
--- patientprofiles/models.py
+++ patientprofiles/models.py
@@ -4,6 +4,21 @@ import datetime
 
 # Create your models here.
 
+# Patient
+class Patient(models.Model):
+  name = models.CharField(max_length=200)
+  birthday = models.DateField("birthday")
+  last_seen = models.DateTimeField("last appointment") 
+  def __str__(self):
+    return self.name
+
+# Ailment
+class Ailment(models.Model):
+  name = models.CharField(max_length=200)
+  description = models.CharField(max_length=2000)
+  def __str__(self):
+    return self.name
+
 # Medication
 class Medication(models.Model):
   name = models.CharField(max_length=200)
@@ -11,19 +26,14 @@ class Medication(models.Model):
   def __str__(self):
     return (self.name)
 
-# Ailment
-class Ailment(models.Model):
-  name = models.CharField(max_length=200)
-  description = models.CharField(max_length=2000)
-  medication = models.ManyToManyField(Medication, blank=True)
-  def __str__(self):
-    return self.name
+# Join table 1
+class PatientAilment(models.Model):
+  patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+  ailment = models.ForeignKey(Ailment, on_delete=models.CASCADE)
+  date_diagnosed = models.DateField("diagnosis date")
 
-# Patient
-class Patient(models.Model):
-  name = models.CharField(max_length=200)
-  birthday = models.DateField("birthday")
-  last_seen = models.DateTimeField("last appointment") 
-  ailments = models.ManyToManyField(Ailment, blank=True)
-  def __str__(self):
-    return self.name
+# Join table 2
+class PatientAilmentMedication(models.Model):
+  patient_ailment = models.ForeignKey(PatientAilment, on_delete=models.CASCADE)
+  medication = models.ForeignKey(Medication, on_delete=models.CASCADE)
+  date_prescribed = models.DateField("prescription date")
